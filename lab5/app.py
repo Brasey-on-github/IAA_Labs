7edb55c14b93d3d07eaf34f11db804140303b167
Loic Brasey <loibrasey@heig-vd.ch>
2024-06-11 20:54:45 +0200

    last stence
    Co-authored-by: Kylian <kylianmanzini@users.noreply.github.com>

--- lab5/app.py
+++ lab5/app.py
@@ -45,64 +45,13 @@ import torch
 from torch import nn
 from torch.nn import Module
 import torch.nn.functional as F
-
-class CNN(Module):
-    def __init__(self):
-        # call the parent constructor
-        super(CNN, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
-        self.conv3 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5)
-        self.conv4 = nn.Conv2d(in_channels=24, out_channels=48, kernel_size=5)
-        self.conv5 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=5)
-
-        self.box_fc1 = nn.Linear(in_features=640, out_features=124)
-        self.box_fc2 = nn.Linear(in_features=124, out_features=64)
-        self.box_out = nn.Linear(in_features=64, out_features=4)
-
-    def forward(self, t):
-        # normalize pixel value
-        t = t / 256
-        t = self.conv1(t)
-        t = F.relu(t)
-        t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        t = self.conv2(t)
-        t = F.relu(t)
-        t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        t = self.conv3(t)
-        t = F.relu(t)
-        t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        t = self.conv4(t)
-        t = F.relu(t)
-        t = F.max_pool2d(t, kernel_size=2, stride=2)
-
-        t = self.conv5(t)
-        t = F.relu(t)
-        t = F.avg_pool2d(t, kernel_size=4, stride=2)
-
-        t = torch.flatten(t,start_dim=-3)
-    
-
-        box_t = self.box_fc1(t)
-        box_t = F.relu(box_t)
-
-        box_t = self.box_fc2(box_t)
-        box_t = F.relu(box_t)
-
-        box_t = self.box_out(box_t)
-        output = F.sigmoid(box_t)
-
-        return output
+from PathFinder import PathFinder
 
 def send_to_stm(socket,x1,y1,x2,y2,dist):
   size = 22 # 2 -> CPX_HEADER + 5 * sizeof(int)
   route = 0xd9 # HOST to STM
   function = 5 # CPX_F_APP
-  packet = struct.pack('<HBBIIIII',size,route,function,x1,y1,x2,y2,dist)
+  packet = struct.pack('<HBBiiiii',size,route,function,x1,y1,x2,y2,dist)
   socket.sendall(packet)
 
 def predict(model , image):
@@ -111,22 +60,16 @@ def predict(model , image):
         image = torch.from_numpy(np.expand_dims(image,axis=1)).float()
         image = image.to(device)
         outputs = model(image)
-        outputs = outputs.cpu().numpy().reshape(4)
     return outputs
 
 def denormalize(coords):   
-    coords[0] = coords[0] * 324
-    coords[1] = coords[1] * 244
-    coords[2] = coords[2] * 324
-    coords[3] = coords[3] * 244
     return coords
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device
 
-model = CNN()
-model.to(device)
-model.load_state_dict(torch.load('pathfinding/nn/model.pth'))
+model = PathFinder.init()
+model.load('./pathfinder3.pth') 
 
 # Args for setting IP/port of AI-deck. Default settings are for when
 # AI-deck is in AP mode.
@@ -156,6 +99,7 @@ import cv2
 
 start = time.time()
 count = 0
+send_to_stm(client_socket,1,2,0,0,0)
 while(1):
     
     # First get the info
@@ -179,18 +123,18 @@ while(1):
      
       count = count + 1
       meanTimePerImage = (time.time()-start) / count
-
-      bayer_img = np.frombuffer(imgStream, dtype=np.uint8)   
-      bayer_img
-      bayer_img.shape = (1,244, 324)
-
       
-      output = predict(model, bayer_img)
-      output = denormalize(output)
-      print(output)
-      ax,ay,bx,by = output
-     
-      send_to_stm(client_socket,int(ax),int(ay),int(bx),int(by),5)
+      bayer_img = np.frombuffer(imgStream, dtype=np.uint8) 
+      
+      
+         
       bayer_img.shape = (244, 324)
+      bayer_img = cv2.cvtColor(bayer_img,cv2.COLOR_BayerBG2GRAY)
+      bayer_img = cv2.flip(bayer_img,0)
+      output = model.get_line_coords(model.preprocess(bayer_img.astype(np.float32)))
+      send_to_stm(client_socket,int(output[0]),0,int(output[1]),int(output[2]),5)
+      
+      bayer_img = cv2.line(bayer_img,(int(output[0]),0),(int(output[1]),int(output[2])),(255,0,0),5)
+      bayer_img = cv2.flip(bayer_img,0)
       cv2.imshow('Raw', bayer_img)
       cv2.waitKey(1)
